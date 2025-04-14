dd9c4e5aa17eff6ec74e28faf0b46cbe6dcb9e60
Berengere TIERTANT <b.lespagnol@gmail.com>
2025-04-14 11:09:35 +0200

    header + home (find all)

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\PokemonRepository;
+
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(PokemonRepository $pokemonRepository): Response
+    {
+        $pokemons = $pokemonRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'pokemons' => $pokemons,
+        ]);
+    }
+}
