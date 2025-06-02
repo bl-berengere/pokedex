4ae7a51f5ad0e217275fbab02f3c478c1f0646e5
Berengere TIERTANT <b.lespagnol@gmail.com>
2025-06-02 12:03:03 +0200

    css responsive

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,17 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.getElementById('search');
     const pokecards = document.querySelectorAll('.pokecard');
+    const noResultsDiv = document.getElementById('no-results');
 
     searchInput.addEventListener('input', (e) => {
         const searchTerm = e.target.value.toLowerCase();
+        let matches = 0;
 
         pokecards.forEach(card => {
             const cardTitle = card.querySelector('.card-title').textContent.toLowerCase();
             if (cardTitle.includes(searchTerm)) {
                 card.style.display = 'block'; // afficher le <a>
+                matches++;
             } else {
                 card.style.display = 'none'; // cacher le <a>
             }
         });
+
+        if (matches === 0) {
+            noResultsDiv.style.display = 'block';
+        } else {
+            noResultsDiv.style.display = 'none';
+        }
     });
 });
